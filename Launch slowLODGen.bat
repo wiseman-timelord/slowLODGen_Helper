4b7dadd0c375be558e282eaddb3af1230e64237b
Ma'Star Wiseman-Timelord <92948927+wiseman-timelord@users.noreply.github.com>
2025-07-28 10:03:40 +0100

    Update Launch slowLODGen.bat

--- Launch slowLODGen.bat
+++ Launch slowLODGen.bat
@@ -1,12 +1,16 @@
 @echo off
+
+# Change to script dir
 cd /d "%~dp0"
 
-:: === 4. admin check ===
+# Admin Check
 fltmc >nul 2>&1 || (
    echo ERROR: Run as Administrator.
    pause & exit /b 1
 )
 
+# Run pypy in script dir
 .\pypy.exe slowLODGen.py 
 
+# Review work
 pause
